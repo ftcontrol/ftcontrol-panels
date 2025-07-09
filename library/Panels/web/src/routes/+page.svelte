184067ed74039b960c22c2b5f8de52cf81718846
lazar <dasidasi86@gmail.com>
2025-07-09 13:51:51 +0300

    got widgets loading

--- library/Panels/web/src/routes/+page.svelte
+++ library/Panels/web/src/routes/+page.svelte
@@ -3,11 +3,19 @@
   import "./global.css"
   import { GlobalSocket } from "ftc-panels/src/core/socket/global"
   import DynamicComponent from "ftc-panels/src/core/ui/DynamicComponent.svelte"
+  import type { GenericData } from "ftc-panels/src/core/socket/types"
+  import type { PluginInfo } from "ftc-panels/src/core/types"
+  import { dev } from "$app/environment"
 
   let socket = new GlobalSocket()
+  let plugins = $state<PluginInfo[]>([])
 
   onMount(() => {
     socket.init()
+
+    socket.addMessageHandler("core", "pluginsDetails", (data: GenericData) => {
+      plugins = data.plugins
+    })
 
     return () => {
       socket.close()
@@ -16,18 +24,20 @@
 </script>
 
 <h1>Hi!</h1>
-
-<DynamicComponent
-  globalSocket={socket}
-  id="com.bylazar.opmodecontrol"
-  file="OpModes"
-/>
-
-<DynamicComponent
-  globalSocket={socket}
-  id="com.bylazar.opmodecontrol"
-  file="Counter"
-/>
+{JSON.stringify(plugins)}
+{#each plugins as plugin}
+  <h2>{plugin.details.name}</h2>
+  <p>{plugin.details.id}</p>
+  <p>{plugin.details.author}</p>
+  {#each plugin.details.widgets as widget}
+    <DynamicComponent
+      isDev={dev}
+      globalSocket={socket}
+      id={plugin.details.id}
+      file={widget.name}
+    />
+  {/each}
+{/each}
 
 <h1>Socket Messages</h1>
 <ul>
