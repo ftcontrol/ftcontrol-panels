3aaf43ce50a5d3bc2f9f7e8059982de8061e6ce9
lazar <dasidasi86@gmail.com>
2025-07-09 03:38:05 +0300

    loading example plugin component

--- library/Panels/web/src/routes/+page.svelte
+++ library/Panels/web/src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import { onMount } from "svelte"
   import "./global.css"
   import { GlobalSocket } from "ftc-panels/src/core/socket/global"
+  import DynamicComponent from "ftc-panels/src/core/ui/DynamicComponent.svelte"
 
   let socket = new GlobalSocket()
 
@@ -15,6 +16,12 @@
 </script>
 
 <h1>Hi!</h1>
+
+<DynamicComponent
+  globalSocket={socket}
+  id="com.bylazar.opmodecontrol"
+  file="opmode"
+/>
 
 <h1>Socket Messages</h1>
 <ul>
