994f9d62a07c3be284a62451b927ad3a17642471
lazar <dasidasi86@gmail.com>
2025-07-09 13:08:38 +0300

    keep widgets inside config

--- library/OpModeControl/web/config.ts
+++ library/OpModeControl/web/config.ts
@@ -7,5 +7,18 @@ export const config: PluginConfig = {
   version: "0.1.0",
   panelsVersion: "0.0.1",
   author: "Unknown",
-  widgets: [],
+  widgets: [
+    {
+      name: "Counter",
+      filepath: "src/widgets/Counter.svelte",
+    },
+    {
+      name: "OpModes",
+      filepath: "src/widgets/OpModes.svelte",
+    },
+    {
+      name: "Counter2",
+      filepath: "src/Counter.svelte",
+    },
+  ],
 }
