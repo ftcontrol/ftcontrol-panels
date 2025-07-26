a58380b58ef364ab4dca935a88b9955b01626178
lazar <dasidasi86@gmail.com>
2025-07-27 01:41:36 +0300

    sidebar work

--- library/Panels/web/src/routes/docs/+layout.svelte
+++ library/Panels/web/src/routes/docs/+layout.svelte
@@ -1,15 +1,66 @@
 <script lang="ts">
   import type { Snippet } from "svelte"
+  import { global } from "$lib"
+  import { Toggle } from "ftc-panels"
 
   let { children }: { children?: Snippet } = $props()
+
+  let coreDocs = $derived(
+    global.plugins.find((it) => it.details.id != "com.bylazar.docs")
+  )
 </script>
 
 <section>
+  <nav>
+    {#if coreDocs != undefined}
+      <Toggle defaultOpen={true}>
+        {#snippet trigger({ isOpen }: { isOpen: boolean })}
+          <p>Core</p>
+        {/snippet}
+        {#snippet content({ close }: { close: () => void })}
+          <div>
+            <p>{coreDocs.details.docs.homepage.name}</p>
+            {#each coreDocs.details.docs.chapters as c}
+              <p>{c.name}</p>
+            {/each}
+          </div>
+        {/snippet}
+      </Toggle>
+    {/if}
+    {#each global.plugins.filter((it) => it.details.id != "com.bylazar.docs") as plugin}
+      <Toggle>
+        {#snippet trigger({ isOpen }: { isOpen: boolean })}
+          <p>{plugin.details.name}</p>
+        {/snippet}
+        {#snippet content({ close }: { close: () => void })}
+          <div>
+            <p>{plugin.details.docs.homepage.name}</p>
+            {#each plugin.details.docs.chapters as c}
+              <p>{c.name}</p>
+            {/each}
+          </div>
+        {/snippet}
+      </Toggle>
+    {/each}
+  </nav>
   {@render children?.()}
 </section>
 
 <style>
+  p {
+    margin: 0;
+  }
+  nav {
+    padding: var(--padding);
+    overflow-y: auto;
+    background-color: red;
+    height: 80vh;
+    width: fit-content;
+  }
   section {
     margin: 0.5rem;
+  }
+  div {
+    margin-left: calc(var(--padding));
   }
 </style>
--- library/Panels/web/src/routes/docs/+page.svelte
+++ library/Panels/web/src/routes/docs/+page.svelte
@@ -1,30 +1,9 @@
 <script lang="ts">
   import { global } from "$lib"
-
-  let coreDocs = $derived(
-    global.plugins.find((it) => it.details.id != "com.bylazar.docs")
-  )
 </script>
 
 <h1>Docs</h1>
 <div>
-  {#if coreDocs != undefined}
-    <p>Core</p>
-    <p>{coreDocs.details.docs.homepage.name}</p>
-    {#each coreDocs.details.docs.chapters as c}
-      <p>{c.name}</p>
-    {/each}
-    <p>---</p>
-  {/if}
-  {#each global.plugins.filter((it) => it.details.id != "com.bylazar.docs") as plugin}
-    <p>{plugin.details.name}</p>
-    <p>{plugin.details.docs.homepage.name}</p>
-    {#each plugin.details.docs.chapters as c}
-      <p>{c.name}</p>
-    {/each}
-    <p>---</p>
-  {/each}
-
   <!-- {#each global.skippedPlugins as plugin}
     <Section>
       <h2>
