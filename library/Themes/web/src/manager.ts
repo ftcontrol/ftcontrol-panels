00c4613c3647e0cfea4b3770f08392265e3cc69b
lazar <dasidasi86@gmail.com>
2025-07-28 23:27:34 +0300

    moved cookies in library

--- library/Themes/web/src/manager.ts
+++ library/Themes/web/src/manager.ts
@@ -1,25 +1,5 @@
-import { PluginManager } from "ftc-panels"
+import { PluginManager, setCookie, getCookie } from "ftc-panels"
 import tinycolor from "tinycolor2"
-
-function setCookie(name: string, value: string, days = 365) {
-  const expires = new Date(Date.now() + days * 864e5).toUTCString()
-  document.cookie = `${name}=${encodeURIComponent(
-    value
-  )}; expires=${expires}; path=/`
-}
-
-function getCookie(name: string): string | null {
-  const cookies = document.cookie
-    .split("; ")
-    .reduce<Record<string, string>>((acc, cur) => {
-      const [key, val] = cur.split("=")
-      if (key && val) acc[key] = decodeURIComponent(val)
-      return acc
-    }, {})
-  const data = cookies[name] ?? null
-
-  return data
-}
 
 type Theme = {
   baseColor: string
