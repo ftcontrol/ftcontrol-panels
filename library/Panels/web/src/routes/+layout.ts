34804465d7f0e989950c4b72b51a3358d2372a14
lazar <dasidasi86@gmail.com>
2025-07-31 00:17:02 +0300

    removed random logs

--- library/Panels/web/src/routes/+layout.ts
+++ library/Panels/web/src/routes/+layout.ts
@@ -4,9 +4,4 @@ import { global } from "$lib"
 export async function load() {
   if (!browser) return
   global.init()
-  setTimeout(() => {
-    console.log(
-      global.plugins.map((it) => it.details.widgets.map((w) => w.name))
-    )
-  }, 1000)
 }
