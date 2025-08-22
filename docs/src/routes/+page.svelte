dca8e0d1a7d8f880e9857406600b89ef40c35449
lazar <dasidasi86@gmail.com>
2025-08-22 05:38:32 +0300

    limelight

--- docs/src/routes/+page.svelte
+++ docs/src/routes/+page.svelte
@@ -4,10 +4,12 @@
     import Plugins from "$lib/ui/sections/Plugins.svelte";
     import HeroCenter from "$lib/ui/sections/HeroCenter.svelte";
     import UpToDate from "$lib/ui/sections/UpToDate.svelte";
+    import Limelight from "$lib/ui/sections/Limelight.svelte";
 </script>
 
 <Hero/>
 <Plugins/>
+<Limelight/>
 <UpToDate/>
 <HeroCenter/>
 <Footer/>
